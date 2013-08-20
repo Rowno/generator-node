d8674a3fde08a53b4ffe5051ddced22de31f8319
Roland Warmerdam <rowno@webspirited.com>
2013-08-20 23:15:11 +1200

    test: switch assertion framework from 'should' to 'chai'

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,9 +18,6 @@ module.exports = function (grunt) {
             ]
         },
         mochacli: {
-            options: {
-                require: ['should']
-            },
             all: []
         }
     });
--- test/index.js
+++ test/index.js
@@ -1,13 +1,16 @@
+/*jshint expr:true */
 'use strict';
 
-var index = require('../lib/index'),
-    fs = require('fs');
+var expect = require('chai').expect;
+var fs = require('fs');
+var index = require('../lib/index');
+
 
 describe('index', function () {
     it('.version should match package.json version', function () {
         var packageJson = fs.readFileSync(__dirname + '/../package.json', 'utf8');
 
         packageJson = JSON.parse(packageJson);
-        index.version.should.equal(packageJson.version);
+        expect(index.version).to.equal(packageJson.version);
     });
 });
