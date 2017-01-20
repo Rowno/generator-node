99ea55126decd1350c84da5e78ea1a905ce0946c
Roland Warmerdam <rowno@webspirited.com>
2017-01-19 23:03:26 -0800

    Fix some linting errors in the templates

--- app/templates/lib/index.js
+++ app/templates/lib/index.js
@@ -3,7 +3,7 @@ const url = require('url')
 
 const DOMAIN = 'localhost'
 
-exports.addDomain = (inputUrl) => {
+exports.addDomain = inputUrl => {
   const parsedUrl = url.parse(inputUrl)
   parsedUrl.host = null
   parsedUrl.hostname = DOMAIN
--- app/templates/test/index.js
+++ app/templates/test/index.js
@@ -3,7 +3,7 @@ const test = require('ava')
 
 const index = require('../lib/index')
 
-test('adds domain to url', (t) => {
+test('adds domain to url', t => {
   t.plan(1)
 
   t.is(index.addDomain('http://127.0.0.1/test/'), 'http://localhost/test/')
