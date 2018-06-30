a2c7370a6ba45e4bf6372b82d53b51140c81f4d7
Roland Warmerdam <roland@segment.com>
2018-06-30 14:27:06 -0700

    Add react example

--- app/templates/react/src/index.js
+++ app/templates/react/src/index.js
@@ -1,3 +1,23 @@
-export default function add(a, b) {
-  return a + b
+import React, {PureComponent} from 'react'
+import PropTypes from 'prop-types'
+
+export default class Button extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    href: PropTypes.string
+  }
+
+  static defaultProps = {
+    href: undefined
+  }
+
+  render() {
+    const {children, href} = this.props
+
+    return href ? (
+      <a href={href}>{children}</a>
+    ) : (
+      <button type="button">{children}</button>
+    )
+  }
 }
--- app/templates/react/test/index.js
+++ app/templates/react/test/index.js
@@ -1,6 +1,20 @@
 import test from 'ava'
-import add from '../src'
+import React from 'react'
+import {shallow} from 'enzyme'
+import Button from '../src'
 
-test('adds domain to url', t => {
-  t.is(add(2, 3), 5)
+test('renders a <button>', t => {
+  const component = shallow(<Button>Test</Button>)
+
+  t.true(component.is('button'))
+  t.is(component.prop('href'), undefined)
+  t.true(component.contains('Test'))
+})
+
+test('renders an <a>', t => {
+  const component = shallow(<Button href="https://roland.codes">Test</Button>)
+
+  t.true(component.is('a'))
+  t.is(component.prop('href'), 'https://roland.codes')
+  t.true(component.contains('Test'))
 })
